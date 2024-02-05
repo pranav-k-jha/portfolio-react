19560a1b26a95d9920c67a3f351de27c7a809822
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-02-05 00:15:50 -0500

    added components and container

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { About, Header, Footer, Testimonial, Skills, Work } from "./container";
+import { Navbar } from "./components";
+import "./App.scss";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
