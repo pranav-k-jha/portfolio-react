7144b1cf553f21b10c8e14343bb0902b6ad63ce1
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-02-05 16:37:23 -0500

    updated app.jsx

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -3,6 +3,7 @@ import { About, Header, Footer, Testimonial, Skills, Work } from "./container";
 import { Navbar } from "./components";
 import "./App.scss";
 
+
 const App = () => {
   return (
     <div className="app">
