46facba23a73f73644a528a75e94766bcf252413
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-02-07 01:16:21 -0500

    added env variables

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,36 +3,49 @@ import { motion } from "framer-motion";
 import { images } from "../../constants";
 
 import "./About.scss";
+import { urlFor, client } from "../../client";
 
 const About = () => {
-  const abouts = [
-    {
-      title: "Fullstack Development",
-      description: "Proficient in Fullstack web development.",
-      imgUrl: images.about01,
-    },
-    {
-      title: "Frontend Development",
-      description: "Passionate about crafting engaging frontend experiences.",
-      imgUrl: images.about02,
-    },
-    {
-      title: "Backend Development",
-      description:
-        "Enjoys tackling intricate challenges in backend development.",
-      imgUrl: images.about03,
-    },
-    {
-      title: "MERN Stack",
-      description:
-        "Experienced in building applications using the MERN (MongoDB, Express.js, React, Node.js) stack.",
-      imgUrl: images.about04,
-    },
-  ];
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
+  // const abouts = [
+  //   {
+  //     title: "Fullstack Development",
+  //     description: "Proficient in Fullstack web development.",
+  //     imgUrl: images.about01,
+  //   },
+  //   {
+  //     title: "Frontend Development",
+  //     description: "Passionate about crafting engaging frontend experiences.",
+  //     imgUrl: images.about02,
+  //   },
+  //   {
+  //     title: "Backend Development",
+  //     description:
+  //       "Enjoys tackling intricate challenges in backend development.",
+  //     imgUrl: images.about03,
+  //   },
+  //   {
+  //     title: "MERN Stack",
+  //     description:
+  //       "Experienced in building applications using the MERN (MongoDB, Express.js, React, Node.js) stack.",
+  //     imgUrl: images.about04,
+  //   },
+  // ];
+
   return (
     <>
       <h2 className="head-text">
-        I know that <span>Good Development</span><br />
+        I know that <span>Good Development</span>
+        <br />
         means <span>Good Business</span>
       </h2>
       <div className="app__profiles">
