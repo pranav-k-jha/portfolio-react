6c682d354eed5a0b82173e31dcb14c08c82870ad
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-02-06 22:38:46 -0500

    added about section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,8 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+
 import "./About.scss";
 
 const About = () => {
-  return <div></div>;
+  const abouts = [
+    {
+      title: "Fullstack Development",
+      description: "Proficient in Fullstack web development.",
+      imgUrl: images.about01,
+    },
+    {
+      title: "Frontend Development",
+      description: "Passionate about crafting engaging frontend experiences.",
+      imgUrl: images.about02,
+    },
+    {
+      title: "Backend Development",
+      description:
+        "Enjoys tackling intricate challenges in backend development.",
+      imgUrl: images.about03,
+    },
+    {
+      title: "MERN Stack",
+      description:
+        "Experienced in building applications using the MERN (MongoDB, Express.js, React, Node.js) stack.",
+      imgUrl: images.about04,
+    },
+  ];
+  return (
+    <>
+      <h2 className="head-text">
+        I know that <span>Good Development</span><br />
+        means <span>Good Business</span>
+      </h2>
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profiles-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
