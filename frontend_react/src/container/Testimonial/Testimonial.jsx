a1a3c6a22d390bcaaecd6dcb7eaaecebb5b1cd75
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-02-06 21:39:38 -0500

    added website under construction

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Testimonial.scss";
 
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  return <div></div>;
 };
 
 export default Testimonial;
--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./Work.scss";
 
 const Work = () => {
-  return <div>Work</div>;
+  return <div style={{color: "red", background: "whitesmoke", margin: "2rem 4rem", padding: "2rem", borderRadius: "25px", boxShadow: "10px 5px 5px red"}}>
+  This website is currently under construction. Please check back later for updates and improvements. Thank you for your patience.</div>;
 };
 
 export default Work;
