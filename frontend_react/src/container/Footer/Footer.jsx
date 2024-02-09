bad07af74ce833e3c806c8ae3bd7d8094bd4afcc
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-02-08 23:29:18 -0500

    updated background color for each sections

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -2,19 +2,9 @@ import React from "react";
 import "./Footer.scss";
 
 const Footer = () => {
-  return  <div
-  style={{
-    color: "red",
-    background: "whitesmoke",
-    margin: "2rem 4rem",
-    padding: "2rem",
-    borderRadius: "25px",
-    boxShadow: "10px 5px 5px red",
-  }}
->
-  This website is currently under construction. Please check back later for
-  updates and improvements. Thank you for your patience. <br /><br /><b style={{color: "black", marginLeft: "84%"}}>... Pranav Jha</b>
-</div>;
+  return (
+    <></>
+  );
 };
 
 export default Footer;
