e2c44a1ce92f8d9ec9d4e67675f823e615c72a07
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-03-04 21:37:36 -0500

    updated linkedin

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -11,7 +11,7 @@ const SocialMedia = () => {
         </a>
       </div>
       <div>
-      <a href="https://www.linkedin.com/in/pranavkjha24" style={{ cursor: "pointer" }}>
+      <a href="https://www.linkedin.com/in/pkrjha" style={{ cursor: "pointer" }}>
         <FaLinkedin />
         </a>
       </div>
