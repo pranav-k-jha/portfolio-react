74eee94f87c658d62c36f0fcc4df39f70b407dae
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-03-25 22:42:17 -0400

    reverted last commit

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
             <span>👋</span>
             <div style={{ marginLeft: 20 }}>
               <p className="p-text">Hello, I am</p>
-              <h1 className="head-text">Pranav <span>Jha</span></h1>
+              <h1 className="head-text">Pranav Jha</h1>
             </div>
           </div>
           <div className="tag-cmp app__flex">
